66a3ea6dc737664a5b8a859e023405ad8b384fba
Prajwal Bharadwaj <133301513+prajwal1798@users.noreply.github.com>
2025-01-10 15:38:16 +0530

    Delete nurbs directory

--- nurbs/nurbsCurveBasicBasisFunctions.m
+++
@@ -1,6 +0,0 @@
-function  nurbs = nurbsCurveBasicBasisFunctions(nurbs)
-
-nurbs.Nu = zeros(1,nurbs.pU+1);
-nurbs.Nu(1) = 1;
-nurbs.aux.leftU = zeros(1,nurbs.pU+1);
-nurbs.aux.rightU = zeros(1,nurbs.pU+1);
--- nurbs/nurbsCurveReverse.m
+++
@@ -1,7 +0,0 @@
-function nurbs = nurbsCurveReverse(nurbs)
-
-nurbs.U = [nurbs.U(1), nurbs.U(1)+cumsum(fliplr(diff(nurbs.U)))];
-nurbs.Pw = flipud(nurbs.Pw);
-
-quadRef = defineQuadratureAdaptive();
-nurbs = nurbsCurveSetupStruct(nurbs, quadRef);
